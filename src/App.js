b174b3ad83c4f59f496e4ffdeeb72a94375b042e
Henry Ball <henryball@Henrys-iMac.local>
2018-12-27 13:14:12 +0000

    add header component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import './App.css';
 
+//components
+import Header from './components/Header';
+
 const App = () => (
-  <p>Five-A-Side App</p>
+  <Header />
 )
 
 export default App;
