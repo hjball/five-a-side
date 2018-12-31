cd5df83dd9f9a7d90b43f308b789f3342b49fa4d
Henry Ball <henry_ball@hotmail.co.uk>
2018-12-31 17:04:02 +0000

    update playerAssign function to randomise

--- src/data/reducers/assignPlayers.js
+++ src/data/reducers/assignPlayers.js
@@ -1,10 +1,13 @@
 import _ from 'lodash';
 
 export default (state, action) => {
+	
+	let shuffle = _.chunk(_.shuffle(state.players), 5);
+	
 	return {
 		// take a copy of state
 		...state, 
-		teamA: _.chunk(_.shuffle(state.players), 5)[0],
-		teamB: _.chunk(_.shuffle(state.players), 5)[1],
+		teamA: shuffle[0],
+		teamB: shuffle[1],
 	}
 };
